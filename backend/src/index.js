bd524220a67206d0336173818c14a34335c6e389
Katriel Koga <katriel.koga.2012@hotmail.com>
2020-03-24 21:56:12 -0300

    criacao Back-end

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const routes = require('./routes')
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes)
+
+/*
+* Rota / Recurso
+*/
+
+/*
+*Metodo http: 
+*
+* GET busca info back-end
+* POST cria info no back-end
+* PUT altera info no back-end
+* DELETE deleta info no back-end
+ */
+
+ /* 
+ * tipos de parametros
+ 
+ * Query: parametros nomeados enviados na rota apos '?' (filtros, paginacao)
+ * Route: parametros para identificar recursos
+ * Body: corpo da requisicao utilizaddo para criar ou alterar recursos
+ *  */
+
+ /**
+  * SQL
+  * NoSQL
+  */
+
+  /**
+   * Driver: SELECT * FROM users
+   * Query Builder: table('users').select('*').where()
+   */
+
+
+
+app.listen(3333);
